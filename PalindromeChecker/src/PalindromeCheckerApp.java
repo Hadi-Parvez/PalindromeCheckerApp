6b0774125398a765a00ac14417e7e0d85a112ee8
Hadi-Parvez <hadi2006parvez@gmail.com>
2026-03-10 08:41:38 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+/*
+Main Class - UseCase1PalindromeApp
+Description:
+This class is the entry point of the palindrome checker app
+At this stage, the application:
+-Starts execution from the main() method
+-Displays a welcome message
+-Shows application version
+No palindrome logic added yet
+@author Hadi Parvez
+@version 1.0
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
